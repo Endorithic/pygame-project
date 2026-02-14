703a281a2547e75f30b31a06837a3d6831b117ce
Endorithic <endorithic@outlook.com>
2026-02-14 22:59:04 +0100

    Added simple comments for `colors.py`

--- src/colors.py
+++ src/colors.py
@@ -1,5 +1,7 @@
+# Type alias for a tuple of integers representing a color in RGB format.
 Color = tuple[int, int, int]
 
+# Simple, predefined color constants
 WHITE: Color = (255, 255, 255)
 BLACK: Color = (0, 0, 0)
 RED: Color = (255, 0, 0)
